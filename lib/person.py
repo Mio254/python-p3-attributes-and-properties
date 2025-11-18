5113422ebe5b477e9387f2194b5fa78ed43225c9
Michael Ogato <michaeloagto884@gmail.com>
2025-11-18 15:21:40 +0300

    Defined a name property for Dog class. Define a breed property for Dog class. Defined a name property for Person class with the name converted to title caseLinks to an external site. before it is saved.
    Defined a job property forPerson class.
    "

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,30 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    
+    def __init__(self, name="John Doe", job="General Management"):
+        self.name = name         # calls name.setter
+        self.job = job           # calls job.setter
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")  
